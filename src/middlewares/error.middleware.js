88da19f46c1d990aa010717ab293d96bc2990c1c
Camara N'famoussa <camaranfamoussa199@gmail.com>
2026-09-10 18:27:32 +0000

    chore(middleware): suppression des commentaires inutilises dans error.middleware.js

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,6 +1,4 @@
 import multer from "multer";
-// import { AppError } from "../shared/errors/AppError.js";
-// import config from "../config/index.js"
 
 export function notFoundHandler(_req, res) {
   res.status(404).json({
@@ -10,30 +8,6 @@ export function notFoundHandler(_req, res) {
 }
 
 export function errorHandler(err, _req, res, _next) {
-  // const isAppError = err instanceof AppError;
-  // const statusCode = isAppError ? err.statusCode : 500;
-  // const code = isAppError ? err.code : 'INTERNAL_ERROR';
-  // const message = isAppError ? err.message : 'Erreur interne du serveur';
-
-  // if (!isAppError && !config.isProduction) {
-  //   console.error(err);
-  // }
-
-  // const payload = {
-  //   success: false,
-  //   error: { code, message },
-  // };
-
-  // if (err.details?.length) {
-  //   payload.error.details = err.details;
-  // }
-
-  // if (!config.isProduction && !isAppError && err.stack) {
-  //   payload.error.stack = err.stack;
-  // }
-
-  // res.status(statusCode).json(payload);
-
 
   if (err instanceof multer.MulterError) {
     if (err.code === "LIMIT_FILE_SIZE") {
